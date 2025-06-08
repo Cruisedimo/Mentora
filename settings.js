1f5cddf436cc7424887cb00a7817ff02fa6c4c9f
Cruise Dimo Tech <cruisedimo@gmail.com>
2025-06-08 10:28:22 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'Mentora',
+  author: 'Cruise Dimo Tech‎',
+  botName: "Mentora",
+  botOwner: 'Cruise', // Your name
+  ownerNumber: '2347044842184', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
